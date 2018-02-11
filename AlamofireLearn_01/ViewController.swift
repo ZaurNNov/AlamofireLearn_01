364458d5be36b950aea900ec80a5ff1080553c02
Zaur Giyasov <zaurusnnov@icloud.com>
2018-02-11 10:28:30 +0300

    first

--- AlamofireLearn_01/ViewController.swift
+++ AlamofireLearn_01/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //request(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Parameters?#>, encoding: <#T##ParameterEncoding#>, headers: <#T##HTTPHeaders?#>)
+        
+//        request("http://jsonplaceholder.typicode.com/posts").responseJSON { response in
+//            print(response)
+//        }
+        
+//        request("http://jsonplaceholder.typicode.com/posts").responseJSON { responseJSON in
+//
+//            guard let statusCode = responseJSON.response?.statusCode else { return }
+//            print("statusCode: ", statusCode)
+//
+//            if (200..<300).contains(statusCode) {
+//                let value = responseJSON.result.value
+//                print("value: ", value ?? "nil")
+//            } else {
+//                print("error")
+//            }
+//        }
+        
+//        request("http://jsonplaceholder.typicode.com/posts").validate().responseJSON { responseJSON in
+//
+//            switch responseJSON.result {
+//            case .success(let value):
+//                print(value)
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+        
+        request("http://jsonplaceholder.typicode.com/posts").responseJSON { responseJSON in
+            
+            switch responseJSON.result {
+            case .success(let value):
+                print("value", value)
+                
+                guard let jsonArray = responseJSON.result.value as? [[String: Any]] else { return }
+                print("array: ", jsonArray)
+                print("1 object: ", jsonArray[0])
+                print("id: ", jsonArray[0]["id"]!)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+        print("viewDidLoad ended")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
